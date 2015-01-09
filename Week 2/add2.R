839672643f8e2e32af939648512381f9984617f0
eneskemalergin <eneskemalergin@gmail.com>
2015-01-09 08:44:43 -0600

    pushing add2 function

---
+++ Week 2/add2.R
@@ -0,0 +1,9 @@
+# My first function in R
+
+add2 <- function(x,y){
+  x + y
+}
+# We don't have do anything special to return 
+# After highlighting the function and run it all 
+# go to console and type add2(3,5)
+# You will get the result 8
