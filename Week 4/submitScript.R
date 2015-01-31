2d8e246729908ee24e885f198dc39afbe3f04e8e
eneskemalergin <eneskemalergin@gmail.com>
2015-01-31 17:14:21 -0600

    Upgraded submitScript

--- Week 4/submitScript.R
+++ Week 4/submitScript.R
@@ -1,4 +1,5 @@
-# 
-source("http://d396qusza40orc.cloudfront.net/rprog%2Fscripts%2Fsubmitscript1.R")
+
+source("http://d396qusza40orc.cloudfront.net/rprog%2Fscripts%2Fsubmitscript3.R")
 submit()
+
 
